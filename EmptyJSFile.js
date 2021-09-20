ff73185a277c4e282619d6265aa0ad6eda172f6e
paulinegiseladenise <90825526+paulinegiseladenise@users.noreply.github.com>
2021-09-20 11:11:44 +0200

    Nyaste CSS

---
+++ EmptyJSFile.js
@@ -0,0 +1,10 @@
+﻿function getname(str) {
+    alert("Hej " + str + " och varmt välkommen!");
+    console.log
+}
+
+var myButton = document.getElementById("myBtn");
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
