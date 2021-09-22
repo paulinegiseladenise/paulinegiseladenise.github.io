7a7d74e0554f2fd80adccf8ed013ea7dfa1fa755
paulinegiseladenise <90825526+paulinegiseladenise@users.noreply.github.com>
2021-09-22 13:54:23 +0200

    testuppdatering
    lagt till testknappar

--- EmptyJSFile.js
+++ EmptyJSFile.js
@@ -1,10 +1,23 @@
-﻿function getname(str) {
-    alert("Hej " + str + " och varmt välkommen!");
-    console.log
-}
-
-var myButton = document.getElementById("myBtn");
+﻿var myButton = document.getElementById("myBtn");
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+
+
+
+
+function tryck() {
+    var answer = document.getElementById("buttonYes").value;
+    document.getElementById("buttonYes").innerHTML = test();
+}
+
+
+function test() {
+    if (answer == "yes")
+        alert ("Kul!");
+}
+
+else {
+alert ("Tråkigt!");
+
 }
