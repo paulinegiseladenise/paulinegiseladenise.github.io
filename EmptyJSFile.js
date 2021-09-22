a2da9b382d652a40883c6e84f9c9f3ca6be0d6c0
paulinegiseladenise <90825526+paulinegiseladenise@users.noreply.github.com>
2021-09-22 16:38:39 +0200

    några förslag för function

--- EmptyJSFile.js
+++ EmptyJSFile.js
@@ -6,6 +6,30 @@ function topFunction() {
 
 
 
+
+
+
+
+    var Ja = jaSvar;
+    var Nej = nejSvar;
+
+    function jaSvar() {
+        alert(Åh vad kul!);
+        document.getElementById("Ja").innerHTML = 'Åh så roligt, då tycker vi likadant!';
+    }
+
+    function nejSvar() {
+        alert(Åh vad trist!);
+        document.getElementById("Nej").innerHTML = 'Nej vad trist. Vi kanske har annat gemensamt!';
+    }
+
+
+    jaSvar();
+    nejSvar();
+
+
+
+
 function tryck() {
     var answer = document.getElementById("buttonYes").value;
     document.getElementById("buttonYes").innerHTML = test();
