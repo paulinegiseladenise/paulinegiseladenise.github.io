2bb03d4bd6443172f22c8945eda639b38811a47a
paulinegiseladenise <90825526+paulinegiseladenise@users.noreply.github.com>
2021-09-24 08:57:59 +0200

    sista checken
    innan presentationen.

--- EmptyJSFile.js
+++ EmptyJSFile.js
@@ -1,10 +1,9 @@
-﻿    var Ja = true;
-    var Nej = false;
+﻿var Ja = true;
+var Nej = false;
 
 
-        function test()
-        {
-            if (Ja = false) {
+function test() {
+    if (Ja = false) {
         alert("Åh vad kul!");
         document.getElementById("Ja").innerHTML = 'Åh så roligt, då tycker vi likadant!';
     }
@@ -13,7 +12,7 @@
         alert("Åh vad trist!");
         document.getElementById("Nej").innerHTML = 'Vi kanske har annat gemensamt!';
     }
-        }
+}
 
 var theButton = document.getElementById("Ja");
 var theButton = document.getElementById("Nej");
@@ -32,27 +31,18 @@ var mittNummer = 5;
 
 function numret() {
     for (var mittNummer = 0; mittNummer < 5; mittNummer++) {
-        
+
         alert("Min forloop som utför något 5 gånger");
     }
 }
 
-    var theNumber = document.getElementById("Min forloop");
+var theNumber = document.getElementById("Min forloop");
 
-    theNumber.addEventListener("click", function () {
-    
-numret();
+theNumber.addEventListener("click", function () {
+
+    numret();
 
 });
 
 
 
-
-
-
-try {
-    nonExistentFunction();
-
-} catch (error) {
-    console.error(error);
-}
